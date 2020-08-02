f78d398d00da38e71c2ba4750c8e5245541f0f8e
Nathan Conlon (MediaMagnet) <nathanec@gmail.com>
2020-08-02 18:33:59 -0500

    Light state is updated off chat user color.

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module Huefighter-go
 go 1.14
 
 require (
+	github.com/amimof/huego v0.0.0-20200504200310-b4a8f71aa2e3
 	github.com/gempir/go-twitch-irc/v2 v2.4.1
+	github.com/jkl1337/go-chromath v0.0.0-20140428033135-240283655afd // indirect
 	github.com/jrm780/gotirc v0.0.0-20180213030644-65759644127d
+	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.7.1
+	gopkg.in/go-playground/colors.v1 v1.2.0
 )
--- huefighter.go
+++ huefighter.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"Huefighter-go/config"
-	"fmt"
+	"github.com/amimof/huego"
 	"github.com/gempir/go-twitch-irc/v2"
+	"github.com/lucasb-eyer/go-colorful"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
 
+	var LColor []float32
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
 	viper.AddConfigPath(".")
+
+
 
 	var cfg config.Configuration
 
@@ -24,16 +29,48 @@ func main() {
 		logrus.Fatalf("unable to decode into struct, %v", err)
 	}
 
+	bridge := huego.New(cfg.Bridge.IP, cfg.Bridge.User)
+	light, err := bridge.GetLights()
+	if err != nil {
+		logrus.Panic(err)
+	}
+	bridge.SetLightState(3, huego.State{
+		On: true,
+		Bri: 255,
+		Sat: 255,
+		HueInc: 255,
+	})
+
 	client := twitch.NewClient(cfg.Twitch.User, cfg.Twitch.OAuth)
-	fmt.Println(cfg.Twitch.User)
 	// client := twitch.NewAnonymousClient()
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		logrus.Infof("[%s, %s] %s \n", message.User, message.User.Color, message.Message)
+		// logrus.Printf("[%v, %v] %v \n", message.User.DisplayName, message.User.Color, message.Message)
+		c, _ := colorful.Hex(message.User.Color)
+		cx, cy, cy2 := c.Xyy()
+		// logrus.Printf("color CIE: %v, %v, %v \n", cx, cy, cy2)
+
+		LColor = append(LColor, float32(cx))
+		LColor = append(LColor, float32(cy))
+		LState, err := bridge.SetLightState(3, huego.State{
+			On:             true,
+			Bri:            uint8(cy2),
+			Xy:             LColor,
+			TransitionTime: 1,
+		})
+		if err != nil {
+			logrus.Error(err)
+		}
+		logrus.Debugln(LState)
+		LColor = make([]float32, 0)
 	})
 
-	client.Join("#mediamagnet")
-	client.OnConnect()
+	client.OnConnect(func() {
+		client.Join(cfg.IRC.Channel)
+		logrus.Infof("%s connected and monitoring %v lights", cfg.Twitch.User, len(light))
+		// client.Say(cfg.IRC.Channel, fmt.Sprintf("HueFighter-Go Connected and working with %v lights.", len(light)))
+	})
+
 
 	err = client.Connect()
 	if err != nil {
